1ccb47d4b2023e2172fe7a9959733b345ec7c535
CelbreX <erik_krekker@gmx.de>
2022-02-28 19:14:00 +0100

    Movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,76 +1,97 @@
 package main;
 
+import entity.Player;
+import tile.TileManager;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel implements Runnable {
-    //Screen Settings
-    final int originalTileSize = 32; //32 x 32 tiles
-    public final int scale = 3;
+public class GamePanel extends JPanel implements Runnable{
+    //Screen settings
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
 
     public final int tileSize = originalTileSize * scale;
     public final int maxScreenCol = 16;
-    public final int maxScreenRow = 10;
+    public final int maxScreenRow = 12;
     public final int screenWidth = tileSize * maxScreenCol;
     public final int screenHeight = tileSize * maxScreenRow;
 
-    int fps = 60;
-    Thread gameThread;
+    //World settings
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxScreenCol;
+    public final int worldHeight = tileSize * maxWorldRow;
 
-    public GamePanel(){
+    //FPS
+    int FPS = 60;
+
+    TileManager tileM = new TileManager(this);
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+    public CollisionChecker cc = new CollisionChecker(this);
+    public Player player = new Player(this, keyH);
+
+    public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
         this.setFocusable(true);
     }
 
     public void startGameThread(){
+
         gameThread = new Thread(this);
         gameThread.start();
     }
 
     @Override
     public void run() {
-        double drawInterval = 1000000000/fps;
-        double delta = 0;
-        long lastTime = System.nanoTime();
-        long currentTime;
-        long timer = 0;
-        int drawCount = 0;
+
+        double drawInterval = 1000000000/FPS;
+        double nextDrawTime = System.nanoTime() + drawInterval;
 
         while(gameThread != null){
 
-            currentTime = System.nanoTime();
-            delta += (currentTime - lastTime)/drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
+            //Update Information
+            update();
 
-            if(delta >= 1){
-                //Update information such as character position
-                update();
-                //Draw updated information
-                repaint();
-                delta--;
-                drawCount++;
-            }
+            //Redraw scene
+            repaint();
 
-            //Prints FPS count
-            if(timer >= 1000000000){
-                //System.out.println(drawCount);
-                drawCount = 0;
-                timer = 0;
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime = remainingTime/1000000;
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += drawInterval;
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
 
     public void update(){
-
+        player.update();
     }
 
     public void paintComponent(Graphics g){
+
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        tileM.draw(g2);
+        player.draw(g2);
 
         g2.dispose();
+
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,22 +3,21 @@ package main;
 import javax.swing.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String [] args){
+
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Super Mario Clone");
+        window.setTitle("My2Dgame");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
         window.pack();
+
         window.setLocationRelativeTo(null);
         window.setVisible(true);
 
         gamePanel.startGameThread();
-
-        //Hallo Erik. Wie gehts?
-        //TODO player movement
     }
 }
